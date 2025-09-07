f7e5018301488841748a728e50dab1ab10f575c2
anjanpoonacha <anjanpoonacha369@gmail.com>
2025-09-07 22:13:26 +0530

    dynamic year in the footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             <footer className="border-t py-6 md:py-0">
               <div className="container mx-auto flex flex-col items-center justify-center gap-4 md:h-16 md:flex-row px-4">
                 <p className="text-center text-sm leading-loose text-muted-foreground">
-                  © 2025 Vishal Thimmaiah. All Rights Reserved.
+                  © {new Date().getFullYear()} Vishal Thimmaiah. All Rights Reserved.
                 </p>
               </div>
             </footer>
